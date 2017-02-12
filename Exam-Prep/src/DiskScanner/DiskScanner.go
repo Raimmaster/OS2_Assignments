5cd8597806d2dc2f99551042cb6f82cebcfc1ed3
Raim <rai.interiano@gmail.com>
2017-02-12 15:12:24 -0600

    FCFS checking algorithm finished

--- Exam-Prep/src/DiskScanner/DiskScanner.go
+++ Exam-Prep/src/DiskScanner/DiskScanner.go
@@ -1,7 +1,17 @@
 package DiskScanner
 
+import (
+  "math"
+  "strings"
+  "strconv"
+)
+
 type DiskScanner struct {
 
+}
+
+func New() *DiskScanner {
+  return &DiskScanner{}
 }
 
 const HEAD_START int = 53
@@ -19,8 +29,17 @@ func (diskScanner *DiskScanner) ScanByOption(addressesArray []string, scanOption
     case 5:
       return cLook(addressesArray, HEAD_START)
   }
+  return 0
 }
 
 func fcfs(addressesArray []string, headStart int) int {
-  
+  var totalHeadMovement int = 0
+  var currentHeadPosition int = headStart
+  for i := 0; i < len(addressesArray); i++ {
+    newHeadPos, _ := strconv.Atoi(strings.TrimSpace(addressesArray[i]))
+    totalHeadMovement += int(math.Abs(float64(newHeadPos - currentHeadPosition)))
+    currentHeadPosition = newHeadPos
+  }
+
+  return totalHeadMovement
 }
